d9069af6e6abcfef90de755254bbbef93939af97
Brandon Turner <brandon88@gmail.com>
2018-10-04 19:48:33 -0700

    5th push

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Handlebars
