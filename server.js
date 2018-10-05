e611f5a4607f9a797eac9ec41ae021b388a2ea6f
Brandon Turner <brandon88@gmail.com>
2018-10-04 19:44:35 -0700

    5th push

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraperz", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Handlebars
 app.engine(
